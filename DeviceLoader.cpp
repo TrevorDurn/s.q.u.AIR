c4a75d69cbfe43cdd322f34e16fd0f973a6b0373
TrevorDurn <tjd2148@columbia.edu>
2024-12-15 13:46:08 -0500

    FlyingFish MQ4 Methane Sensor Files

---
+++ DeviceLoader.cpp
@@ -0,0 +1,32 @@
+#include "DeviceLoader.h"
+
+/// @brief Loads all event receivers
+/// @return True on success
+bool DeviceLoader::LoadReceivers() {
+
+    /******** Add event receivers and loggers here ********/
+    
+    Logger.addReceiver(&slogger);
+    EventBroadcaster::addReceiver(&led);
+
+    /******** End event receivers and loggers addition section ********/
+    return true;
+}
+
+/// @brief Loads all sensor and actor devices
+/// @return True on success
+bool DeviceLoader::LoadDevices() {
+    
+    /******** Add senors and actors here ********/ 
+    // actors are pumps etc
+
+
+    ActorManager::addActor(&reset);
+    ActorManager::addActor(&logger);
+    SensorManager::addSensor(&bme280);   //add new sensors here
+    SensorManager::addSensor(&pms5003); 
+    SensorManager::addSensor(&methanesensor);
+             
+ /******** End senor and actor addition section ********/
+    return true;
+}
